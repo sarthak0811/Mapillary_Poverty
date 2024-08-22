2f4b1df1d9550a0359220a4e9bef0efad5dc45c3
Sarthak Sachdev <92429357+sarthak0811@users.noreply.github.com>
2024-08-22 18:42:46 +0530

    Delete seamseg-main directory

--- seamseg-main/seamseg/utils/bbx/_backend.pyi
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-
-import torch
-
-
-def extract_boxes(mask: torch.Tensor, n_instances: int) -> torch.Tensor: ...
-
-
-def mask_count(bbx: torch.Tensor, int_mask: torch.Tensor) -> torch.Tensor: ...
--- seamseg-main/seamseg/utils/nms/_backend.pyi
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-
-import torch
-
-
-def nms(bbx: torch.Tensor, scores: torch.Tensor, threshold: float, n_max: int) -> torch.Tensor: ...
